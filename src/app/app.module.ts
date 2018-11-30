0ee303c5b5d79be333a6b374ee7fefbe9922b948
Farshid Kamal <tofarshid@gmail.com>
2018-11-30 14:37:32 +1100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,39 +3,64 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MyNavComponent } from './my-nav/my-nav.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { HomeComponent } from './home/home.component';
+// about
+import { MatTabsModule } from '@angular/material/tabs';
+// course
+// course list
+import {
+    MatDatepickerModule,
+    MatDialogModule,
+    MatInputModule, MatListModule, MatPaginatorModule, MatProgressSpinnerModule, MatSelectModule, MatSidenavModule,
+    MatSortModule,
+    MatTableModule,
+    MatToolbarModule
+} from "@angular/material";
+import {HttpClientModule} from "@angular/common/http";
+// course dialogue
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+// import {MatMomentDateModule} from "@angular/material-moment-adapter"; ???????
+
 import { LayoutModule } from '@angular/cdk/layout';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { 
-  MatToolbarModule, 
-  MatButtonModule, 
-  MatSidenavModule, 
-  MatIconModule, 
-  MatListModule,
-  MatCardModule
-} from '@angular/material';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    MyNavComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatCardModule,
-    FlexLayoutModule
+        BrowserAnimationsModule,
+        HttpClientModule,
+        MatMenuModule,
+        MatButtonModule,
+        MatIconModule,
+        MatCardModule,
+        MatTabsModule,
+        MatSidenavModule,
+        MatListModule,
+        MatToolbarModule,
+        MatInputModule,
+        MatTableModule,
+        MatPaginatorModule,
+        MatSortModule,
+        MatProgressSpinnerModule,
+        MatDialogModule,
+        AppRoutingModule,
+        MatSelectModule,
+        MatDatepickerModule,
+        ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  // entryComponent: [CourseDialogComponent]
 })
 export class AppModule { }
